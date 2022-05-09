f5d13aaf827096457cf5cf19390ff3961a3e10e7
Alberto Ceballos <alceballosa@unal.edu.co>
2022-05-09 18:42:31 -0400

    Remove unused imports

--- environment/settings.py
+++ environment/settings.py
@@ -1,13 +1,12 @@
-import os
-from os.path import dirname
-from os.path import join
-
+# import os
+# from os.path import dirname
+# from os.path import join
 # from dotenv import load_dotenv
 
-#dotenv_path = join(dirname(__file__), os.getenv("ENVIRONMENT_FILE"))
-#load_dotenv(dotenv_path=dotenv_path, override=True)
+# dotenv_path = join(dirname(__file__), os.getenv("ENVIRONMENT_FILE"))
+# load_dotenv(dotenv_path=dotenv_path, override=True)
 
-APP_HOST = "127.0.0.1" # os.environ.get("HOST")
-APP_PORT = 8085 # int(os.environ.get("PORT"))
-APP_DEBUG = True # bool(os.environ.get("DEBUG"))
-DEV_TOOLS_PROPS_CHECK = True # bool(os.environ.get("DEV_TOOLS_PROPS_CHECK"))
+APP_HOST = "127.0.0.1"  # os.environ.get("HOST")
+APP_PORT = 8085  # int(os.environ.get("PORT"))
+APP_DEBUG = True  # bool(os.environ.get("DEBUG"))
+DEV_TOOLS_PROPS_CHECK = True  # bool(os.environ.get("DEV_TOOLS_PROPS_CHECK"))
